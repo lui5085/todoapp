8f46151ffa02c25cbe376e952dca822a9f5382a6
lui5085 <luis.albuquerque@alu.ufc.br>
2025-01-11 15:43:09 -0300

    :sparkles: feat: todo item functional

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,33 @@
+class ItemsController < ApplicationController
+
+    def create
+        @item = Item.create(item_params)
+
+
+        if @item.save
+            redirect_to todo_list_path(@item.todo_list_id)
+        else
+            redirect_to root_path
+        end  
+    end
+
+    def update
+        @item = Item.find(params[:id])
+        if @item.update(item_params)
+            redirect_to todo_list_path(@item.todo_list_id)
+        else
+            redirect_to root_path
+        end
+    end
+
+
+    private
+
+    def item_params
+        params.require(:item).permit(:title, :conclued, :todo_list_id)
+    end
+
+
+
+
+end
